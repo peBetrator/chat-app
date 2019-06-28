4d6f42b26cfa8e07300f619260eaa73d035f89bd
mnegal <maxim.negaliuc@endava.com>
2019-06-28 14:43:08 +0300

    chat app

---
+++ src/Input.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import React from "react";
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  onChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault(); //prevent page reloading
+    this.setState({ text: "" });
+    this.props.onSendMessage(this.state.text);
+  }
+
+  render() {
+    return (
+      <div className="Input">
+        <form onSubmit={e => this.onSubmit(e)}>
+          <input
+            onChange={e => this.onChange(e)}
+            value={this.state.text}
+            type="text"
+            placeholder="Enter your message and press ENTER"
+            autoFocus={true}
+          />
+          <button>Send</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Input;
